790f4d50605b8b86592c295ac3c27deef8e55759
Ericomp <eri.ichik@gmail.com>
2021-02-12 18:19:45 +0000

    port from environment variable

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ var express = require('express');
 //making an express application
 //store the result of that in a variable called app
 var app = express();
-var server = app.listen(3000);
+
+
+const port = process.env.PORT || 3000;
+// var server = app.listen(port);
+var server = app.listen(port, () => {
+    console.log(`Starting server at ${port}`)
+});
 
 app.use(express.static('public'));
 
