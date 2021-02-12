9b46e77d23c45873cb864c5da8fa78c52bf2277e
Ericomp <eri.ichik@gmail.com>
2021-02-12 17:43:59 +0000

    node websockets template

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('My socket server is running')
+
+//importing the module using (module name =express)
+//triger the express function
+var express = require('express');
+
+//making an express application
+//store the result of that in a variable called app
+var app = express();
+var server = app.listen(3000);
+
+app.use(express.static('public'));
+
+
+console.log("My socket server is running");
