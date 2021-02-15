7fedc9eabc973782e3b5906a2c9a9a0cb3b13874
Ericomp <eri.ichik@gmail.com>
2021-02-15 15:00:08 +0000

    first commit

--- public/sketch.js
+++ public/sketch.js
@@ -1,12 +1,44 @@
+var posX, posY;
+var amt;
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
+  background(0, 0, 0);
+  colorMode(HSB, 100);
 }
 
 function draw() {
-	background(51);
-	ellipse(mouseX,mouseY,60);
+
+  push();
+
+  posX = mouseX;
+  posY = mouseY;
+  amt = random(10, 100);
+
+  var nPosX = posX + random(-amt, amt);
+  var nPosY = posY + random(-amt, amt);
+
+  //strokeWeight(random(0.1,2));
+  noStroke();
+  fill(0, 0, 100, 3);
+  //ellipse(posX, posY, nPosX, nPosY);
+  rect(mouseX, mouseY, 50 + random(-amt, amt), 100 + random(-amt, amt));
+  rotate(30 * PI / 180);
+
+  posX = nPosX;
+  posY = nPosY;
+
+  pop();
 }
 
-function windowResized() { 
-    resizeCanvas(windowWidth, windowHeight); 
-} 
+function mousePressed() {
+
+  fill(random(255), 10, 80);
+  rect(0, 0, windowWidth, windowHeight);
+
+}
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
